12f9c24104be88f6ab09ed4006aa7bd6246eb28a
PukPukov <vovanchikputin@vk.com>
2024-03-25 21:45:11 +0500

    prevent creating war-disabled cities

--- src/main/java/ru/ancap/states/listeners/StatesListener.java
+++ src/main/java/ru/ancap/states/listeners/StatesListener.java
@@ -7,6 +7,7 @@ import org.bukkit.event.Listener;
 import ru.ancap.commons.debug.AncapDebug;
 import ru.ancap.commons.exception.UnsafeThread;
 import ru.ancap.framework.communicate.message.CallableMessage;
+import ru.ancap.hexagon.Hexagon;
 import ru.ancap.states.AncapStates;
 import ru.ancap.states.dynmap.DynmapDrawer;
 import ru.ancap.states.event.events.*;
@@ -17,6 +18,11 @@ public class StatesListener implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void on(CityFoundEvent event) {
         event.getHost().initialize(event.getCreator(), event.getName());
+        for (Hexagon hexagon : event.getHost().getHomeHexagon().neighbors(1)){
+            if (AncapStates.getCityMap().getCity(hexagon) != null) continue;
+            event.getHost().addHexagon(hexagon);
+            break; // claim only one
+        }
         DynmapDrawer.redrawDynmap();
         CallableMessage message = LStateMessage.CITY_CREATE(event.getCreator().getName(), event.getName());
         AncapStates.sendMessage(message);
