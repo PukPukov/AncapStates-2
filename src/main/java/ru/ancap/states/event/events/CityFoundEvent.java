8ca816939838290dc10e423422cb2a838b196167
PukPukov <kvasov.les@gmail.com>
2025-04-05 22:52:51 +0500

    this event in fact is not cancellable

--- src/main/java/ru/ancap/states/event/events/CityFoundEvent.java
+++ src/main/java/ru/ancap/states/event/events/CityFoundEvent.java
@@ -2,8 +2,6 @@ package ru.ancap.states.event.events;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
@@ -12,10 +10,8 @@ import ru.ancap.states.states.city.City;
 
 @Getter
 @RequiredArgsConstructor
-public class CityFoundEvent extends Event implements Cancellable {
+public class CityFoundEvent extends Event {
 
-    @Setter
-    private boolean cancelled;
     private final AncapStatesPlayer creator;
     private final String name;
     private final City host;
